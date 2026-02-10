1f7af4e5787f6a1b31d7bbec622c1f84f8b25e02
Philip Segynola <philipsegynola@gmail.com>
2026-02-10 14:46:09 -0700

    iam.tf errors

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -25,7 +25,7 @@ data "aws_iam_policy_document" "tf_backend" {
     effect  = "Allow"
     actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
     resources = [
-        # allow the exact state file object
+      # allow the exact state file object
       "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy",
 
       # allow variants/prefixes if you ever use them
